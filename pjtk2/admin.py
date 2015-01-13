66349374eecb4fc3b89fa02ea6f8c8a6fda31a01
Adam Cottrill <adam.cottrill@ontario.ca>
2015-01-13 16:01:47 -0500

    Updates to Pjtk2 admin
    small improvements to ptjk2 admin pages - mostly associated with
    projects.  Added more columns to be displayed and some filters.

--- pjtk2/admin.py
+++ pjtk2/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from django.contrib.auth import User
+
 from pjtk2.models import (Milestone, ProjectType, Database, Project,
                           Report, Lake, Family, Employee, ProjectMilestones,
                           ProjectSisters, Message, Messages2Users)
@@ -18,34 +20,43 @@ class Admin_ProjectType(admin.ModelAdmin):
     '''Admin class for Project Types'''
     list_display = ('project_type', 'field_component',)
 
+
 class Admin_Database(admin.ModelAdmin):
     '''Admin class for databases'''
     pass
+
 
 class Admin_Lake(admin.ModelAdmin):
     '''Admin class for lakes'''
     pass
 
+
 class Admin_Project(admin.ModelAdmin):
     '''Admin class for Projects'''
-    pass
+    list_display = ('year', 'prj_cd', 'prj_nm','prj_ldr', 'project_type')
+    list_filter = ('project_type','year', 'prj_ldr', 'lake')
+
 
 class Admin_Family(admin.ModelAdmin):
     '''Admin class for familiy table'''
     pass
 
+
 class Admin_ProjectSisters(admin.ModelAdmin):
     '''Admin class for project-sisters table'''
     pass
+
 
 class Admin_ProjectMilestones(admin.ModelAdmin):
     '''Admin class for project - milestones'''
     list_display = ('project', 'milestone',)
     list_filter = ('project', 'milestone')
 
+
 class Admin_Report(admin.ModelAdmin):
     '''Admin class for reprorts'''
     list_display = ('current', 'report_path', 'uploaded_on', 'uploaded_by')
+
 
 class Admin_Employee(admin.ModelAdmin):
     '''Admin class for Employees'''
@@ -70,7 +81,6 @@ class Admin_Messages2Users(admin.ModelAdmin):
 
     def milestone(self, obj):
         return obj.message.project_milestone.milestone
-
 
 
 
